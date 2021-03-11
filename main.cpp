902fef0f8d2e581b2df47f263ed85178819164ef
jormungandr3000 <64022332+jormungandr3000@users.noreply.github.com>
2021-03-11 21:30:09 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,80 +1,30 @@
 #include <iostream>
-#include <fstream>
 #include <string>
-#include <vector>
-#include <sstream>
 
-std::vector<std::string> split(std::string sor);
+std::string strip(std::string string1);
 
 int main() {
+    std::cout<<strip(".,...../.,Clovo,,/.....");
+    return 0;
+}
+
+std::string strip(std::string string1){
     std::string last;
-    std::string min;
-    std::string max;
-    char la1;
-    int z;
-    int y;
-    std::vector<int> sh2;
-    std::vector<int> sh1;
-    std::string b = "./input.txt";
-    std::string a;
-    std::ifstream file(b);
-    if (!file) {
-        std::cout << "file wasn't open" << std::endl;
-    } else {
-        std::cout << "file was open" << std::endl;
-    }
-    while (!file.eof())
-    {
-        getline(file,a);
-        last.push_back(' ');
-        for (int i=0; i < a.length(); ++i) {
-            la1 = a[i];
-            last.push_back(la1);
+    while((!isalpha(string1[0]))){
+        if (!isalpha(string1[0])){
+            for (int i=1; i<string1.length()+1; ++i)
+                last.push_back(string1[i]);
+            string1 = last;
         }
+        last="";
     }
-    //getline(file, a);
-
-    std::cout << last << std::endl;
-    for (int i = 0; i < sh1.size(); ++i)
-        sh2.push_back(split(a)[i].length());
-
-    /*for (int i = 0; i < sh1.size(); ++i){
-        if (sh2[i]>max){
-            max = sh2[i];
-            z = i;
+    while((!isalpha(string1[string1.length()-1]))){
+        if (!isalpha(string1[string1.length()-2])){
+            for (int i=0; i<string1.length()-2; ++i)
+                last.push_back(string1[i]);
+            string1 = last;
         }
-        if (sh2[i]<min){
-            min = sh2[i];
-            y = i;
-        }
+        last="";
     }
-    */
-    std::vector<std::string> words = split(a);
-
-
-    for (auto word: words)
-        if (word.length()>max.length()){max = word;};
-    min = max;
-    for (auto word: words)
-        if (word.length()<min.length()){min = word;};
-
-    std::cout<<min<<"     " <<max;
+    return string1;
 }
-
-std::vector<std::string> split(std::string str){
-    std::vector<std::string> sh;
-    std::string buf;
-    std::stringstream ss(str);
-
-    std::vector<std::string> tokens;
-
-    while (ss >> buf)
-        tokens.push_back(buf);
-
-    for (auto &st:tokens) {
-        sh.push_back(st);
-    }
-    return sh;
-
-}
-
