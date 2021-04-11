e4d97094500ba786c56520eca9d77bb2c75e589b
Mykola Morgunenko <Olli25010@gmail.com>
2021-04-11 20:58:06 +0200

    add comments

--- main.cpp
+++ main.cpp
@@ -5,12 +5,13 @@
 #include <sstream>
 
 
-std::vector<std::string> preprocessing(std::string path);
+std::vector <std::string> preprocessing(std::string path);
 
 std::string strip(std::string string1);
 
 // the name of var's have to be the same
-std::vector<std::string> split(std::string str);
+std::vector <std::string> split(std::string str);
+
 //
 int main(int argc, char *argv[]) {
     std::string last;
@@ -39,11 +40,10 @@ int main(int argc, char *argv[]) {
     } else {
         std::cout << "file was open" << std::endl;
     }
-    while (!file.eof())
-    {
-        getline(file,a);
+    while (!file.eof()) {
+        getline(file, a);
         last.push_back(' ');
-        for (int i=0; i < a.length(); ++i) {
+        for (int i = 0; i < a.length(); ++i) {
             la1 = a[i];
             last.push_back(la1);
         }
@@ -65,36 +65,36 @@ int main(int argc, char *argv[]) {
         }
     }
     */
-    std::vector<std::string> words = split(last);
+    std::vector <std::string> words = split(last);
 
 
     for (auto word: words)
-        if (word.length()>max.length()){max = word;};
+        if (word.length() > max.length()) { max = word; };
     min = max;
     for (auto word: words)
-        if (word.length()<min.length()){min = word;};
+        if (word.length() < min.length()) { min = word; };
 
-    std::cout<<min<<"     " <<max<<std::endl;
+//    std::cout << min << "     " << max << std::endl;
 
-    for (auto word: words)
-        std::cout<<strip(word)<<std::endl;
+//    for (auto word: words)
+//        std::cout << strip(word) << std::endl;
 
-    std::vector<std::string> words3 = preprocessing("./test1.txt");
+    std::vector <std::string> words3 = preprocessing("./test1.txt");
 
-    for (auto word: words3) {
-        std::cout << word << "   ";
-        result += 1;
-    }
-    std::cout<<std::endl;
-    std::cout<<"result of the last task:  "<<result;
+//    for (auto word: words3) {
+//        std::cout << word << "   ";
+//        result += 1;
+//    }
+    std::cout << std::endl;
+    std::cout << "result of the last task:  " << result;
 }
 
-std::vector<std::string> split(std::string str){
-    std::vector<std::string> sh;
+std::vector <std::string> split(std::string str) {
+    std::vector <std::string> sh;
     std::string buf;
     std::stringstream ss(str);
 
-    std::vector<std::string> tokens;
+    std::vector <std::string> tokens;
 
     while (ss >> buf)
         tokens.push_back(buf);
@@ -106,13 +106,13 @@ std::vector<std::string> split(std::string str){
 
 }
 
-std::string strip(std::string string1){
-    if (string1.length()!=1) {
-        std::string las;
+std::string strip(std::string string1) { // better name is input_string
+    if (string1.length() != 1) {
+        std::string las; // I didn`t understand the meaning of this variable =)
         while ((!isalpha(string1[0]))) {
             if (!isalpha(string1[0])) {
                 for (int i = 1; i < string1.length() + 1; ++i)
-                    las.push_back(string1[i]);
+                    las.push_back(string1[i]); // better use string stream (more in the tasks I asked to make)
                 string1 = las;
             }
             las = "";
@@ -128,40 +128,62 @@ std::string strip(std::string string1){
             }
         }
     }
+    /*
+     * How to do better? (if you know lambda functions and iterators (we will learn that =)))
+void ltrim(std::string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+        return std::isalpha(ch);
+    }));
+}
+
+
+static inline void rtrim(std::string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+        return !std::isalpha(ch);
+    }).base(), s.end());
+}
+
+static inline void strip(std::string &s) {
+    ltrim(s);
+    rtrim(s);
+}
+     *
+     * */
     return string1;
 }
 
 
 
-
-//Last task
-std::vector<std::string> preprocessing(std::string path){
+//Last task // not clear name, what is the functional of the function?
+///////////////////////////////?
+// Where formatting was??? CLion can make a perfect formatting for you, just press Ctrl+Alt+l
+///////////////////////////////?
+std::vector <std::string> preprocessing(std::string path) {
     std::string z;
     std::ifstream file02(path);
-    char la02;
+    char la02; // ?? what for
     std::string last02;
     std::string last01 = "";
-while (!file02.eof())
-{
-getline(file02,z);
-last01.push_back(' ');
-for (int i=0; i < z.length(); ++i) {
-la02 = z[i];
-last01.push_back(la02);
-}
-}
-for(int i=0; i<last01.length(); ++i){
-    if (isalpha(last01[i]))
-    last02.push_back(tolower(last01[i]));
-}
-std::vector<std::string> words2 = split(last02);
+    while (!file02.eof()) {
+        getline(file02, z);
+        last01.push_back(' ');
+        for (int i = 0; i < z.length(); ++i) {
+            la02 = z[i];
+            last01.push_back(la02);
+        } // lines 170-173 - you just copy the line char by char. why?
+    }
+    for (int i = 0; i < last01.length(); ++i) {
+        if (isalpha(last01[i]))
+            last02.push_back(tolower(last01[i])); // bad practice. + you forget to add spaces, so split will not work =)
+    }
+    std::vector <std::string> words2 = split(last02);
 
 
-for (auto &word: words2) {
-    std::cout<<word<<"  ";
-    word = strip(word);
-}
-return words2;
+    for (auto &word: words2) {
+        std::cout << word << " ";
+        word = strip(word);
+    }// as you can see here. split fn doesn`t did it`s job, because to the input you gave one word....
+    return words2;
 
 }
 
